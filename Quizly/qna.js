194cb2b56d22f894d48fc9f69a5536e973ae6455
banipreet1 <banipreet987@gmail.com>
2023-09-29 19:37:05 +0530

    quizly

---
+++ Quizly/qna.js
@@ -0,0 +1,29 @@
+// question
+
+var que = ["what real name of India",
+ "which of capital of Sikkim",
+  'which country have good relation with India ',
+  'which is smallest ocean in the world',
+  'which country gifted the statue of liberty to USA in 1886',
+  'which country is known as the playground of Eeroupe',
+  'Which country is known as the Land of Rising Sun',
+  'Which country is known as the Land of Thunderbolt',
+  'Which continent has highest number of countries',
+  "white elephant found in which country",
+  "Which is the biggest island in the world "]
+
+
+// answer
+var ans = ["bharat",
+ "gangtok",
+  "bhutan",
+  'arctic',
+  'france',
+  'switerland',
+  'japan',
+  'bhutan',
+  'africa',
+  'thailand',
+  'greenland',
+ '']
+
