777751e27590eadf0da37c4fae937689ccaf51dc
banipreet1 <banipreet987@gmail.com>
2023-10-19 19:15:29 +0530

    properly deploy my bike website

---
+++ My Bike/js/index.js
@@ -0,0 +1,13 @@
+function bicycle(){
+    document.getElementById('bic').style.display ="block";
+    document.getElementById('acc').style.display ="none";
+    document.getElementById('btnbic').style.display ="none";
+    document.getElementById('btnacc').style.display ="block";
+    
+}
+function accs(){
+    document.getElementById('bic').style.display ="none";
+    document.getElementById('acc').style.display ="block";
+    document.getElementById('btnbic').style.display ="block";
+    document.getElementById('btnacc').style.display ="none";
+}
